9ff483c3355ae7243a75798b5f0c415340598f7c
imSpewds <titorific10@gmail.com>
2019-12-03 09:41:49 -0800

    python edit

--- webapp.py
+++ webapp.py
@@ -18,7 +18,7 @@ def get_state_options():
     
 @app.route("/")
 def render_main():
-    return render_template('index.html', responseFromServer = get_state_options())
+    return render_template('index.html')
     
 if __name__=="__main__":
     app.run(debug=False)
