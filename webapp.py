6bc45e5727167f2febde0dbaa4ee9a7946eef094
BuildTools <unconfigured@null.spigotmc.org>
2019-12-09 18:18:52 -0800

    edited python

--- webapp.py
+++ webapp.py
@@ -23,7 +23,7 @@ def get_state_facts(state):
     fact = ""
     for county in counties:
         if state == county['State']:
-            income = county['Income']['Median Houseold Income']
+            income = county['Income']['Median Household Income']
     fact = Markup("<p>" + "Median household income for " + state + " is " + "$" + income + "</p>")
     return fact
     
